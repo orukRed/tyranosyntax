00b580c68d56436b76e85ebc7e730f371dca9b12
orukRed <orukRed@gmail.com>
2021-07-06 00:28:35 +0900

    fix;コメントアウト追加

--- src/TagHoverProvider.ts
+++ src/TagHoverProvider.ts
@@ -7,9 +7,9 @@ export class TagHoverProvider{
 
 	constructor(){
 		this.jsonTyranoSnippet = JSON.parse(fs.readFileSync(__dirname+"./../snippet/tyrano.snippet.json","utf8"));
+		
+		//FIXME:ダブルクォーテーションで囲まないと正しくツールチップが表示されない。
 		this.regExp = /(\w+)(\s*((\w*)=\"(\S*)\")*)*/;//取得した行に対しての正規表現
-
-
 	}
 
 	private createMarkdownText(textValue:string) : vscode.MarkdownString | null{
