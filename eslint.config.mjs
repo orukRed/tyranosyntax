4b17273933fb51b6dee4bad387909989b1e23175
botamochi0x12 <botamochi0x12@gmail.com>
2024-07-08 02:23:06 +0900

    chore: `eslint` と `prettier` を使用
    chore: Use `eslint` and `prettier`
    
    ---
    
    Signed-off-by: botamochi0x12 <botamochi0x12@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// https://eslint.org/
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
