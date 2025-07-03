de117e1140abd44e50f53f2d846e7e230538ece7
orukRed <orukred@gmail.com>
2025-07-03 20:04:13 +0900

    fix:parser.test.tsのエラー修正

--- src/test/suite/Parser.test.ts
+++ src/test/suite/Parser.test.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-import { InformationWorkSpace } from "../../InformationWorkSpace";
-import path from "path";
+// import { InformationWorkSpace } from "../../InformationWorkSpace";
+// import path from "path";
 import { Parser } from "../../Parser";
 // import * as myExtension from '../../extension';
 
